27378147920324a6773f96e5c19d6ebf68948e03
Cas van Dongen <cas.vandongen@student.fontys.nl>
2017-02-10 21:52:30 +0100

    Added /pcn, /rooster and /help command

--- bot.py
+++ bot.py
@@ -1,18 +1,32 @@
-from telegram.ext import Updater, Filters, BaseFilter, MessageHandler
+from telegram.ext import Updater, Filters, BaseFilter, MessageHandler, CommandHandler
 
 # Set token
 updater = Updater("")
 dispatcher = updater.dispatcher
 
-# Set all commands
+# rooster command
 def txtGetRooster(bot, update):
     #TODO: Check if user exists in database
-    message = "Voordat ik je je rooster voor vandaag kan geven moet ik eerst weten in welke klas je zit. Dit kun je instellen door het volgende commando uit te voeren: /klas \"klasnummer\"."
+    message = "Voordat ik je je rooster voor vandaag kan geven moet ik eerst weten in welke klas je zit. Dit kun je laten weten door je pcn op te geven, dit kan met het volgende commando: /pcn \"000000\"."
     bot.sendMessage(chat_id=update.message.chat_id, text=message)
+
+# PCN command
+def cmdPcn(bot, update, args):
+    arguments = ' '.join(args).upper()
+    if len(arguments) > 1 or len(arguments) < 1:
+        message = "Je gebruikt het commando niet helemaal correct. Probeer het opnieuw als /pcn \"000000\""
+    else:
+        #save pcn to databse with chat user id or slug
+        message = "Top!"
+    bot.sendMessage(chat_id=update.message.chat_id, text=message)
+
+# Help command
+def cmdHelp(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="De volgende commando's kun je gebruiken:\r\n\r\n/pcn \"000000\" - PCN nummer instellen om rooster te bepalen\r\n/rooster - Om je rooster op te halen voor vandaag, je kunt ook gewoon het woord rooster in een zin gebruiken.\r\n/help, /cmd of /commands - Huidige uitleg.")
 
 # Unknown text, please try again
 def unknownCmd(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, ik snap niet wat je bedoelt.")
+    bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, ik snap niet wat je bedoelt. Vraag om hulp óf gebruik het commando /help")
 
 # Define custom filter
 class FilterRooster(BaseFilter):
@@ -20,8 +34,19 @@ class FilterRooster(BaseFilter):
         return 'rooster' in message.text.lower()
 filterRooster = FilterRooster()
 
-# Add handler to updater for unknown command
+# Add handler to dispatcher for rooster command
 dispatcher.add_handler(MessageHandler(filterRooster, txtGetRooster))
+dispatcher.add_handler(CommandHandler('rooster', txtGetRooster))
+
+# Add handler to dispatcher for PCN command
+dispatcher.add_handler(CommandHandler('pcn', cmdPcn, pass_args=True))
+
+# Add handler to dispatcher for /help command
+dispatcher.add_handler(CommandHandler('help', cmdHelp))
+dispatcher.add_handler(CommandHandler('cmd', cmdHelp))
+dispatcher.add_handler(CommandHandler('commands', cmdHelp))
+
+# Add handler to dispatcher for unknown command
 dispatcher.add_handler(MessageHandler((Filters.text | Filters.command), unknownCmd))
 
 # Let it run continuously
